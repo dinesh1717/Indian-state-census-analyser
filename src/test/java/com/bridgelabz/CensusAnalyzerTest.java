8d6e8820fc65a4c0917cf970ac997261422906dc
dinesh nangare <dineshnangare1717@gmail.com>
2022-05-02 23:58:41 +0530

    incorrect header

--- src/test/java/com/bridgelabz/CensusAnalyzerTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyzerTest.java
@@ -4,19 +4,19 @@ import org.testng.annotations.Test;
 
 
 public class CensusAnalyzerTest {
+    private static final String INDIA_CENSUS_CSV_PATH_HEADER = "F:\\indian-state-census-analyzer\\src\\main\\resources\\IndiaStateCensusDataHeader.csv";
 
-
-    private static final String INDIA_CENSUS_CSV_PATH_DELIMETER = "F:\\indian-state-census-analyzer\\src//main\\resources\\IndiaStateCensusData.pdf";
-
-    //UseCase 1.4 Path is correct but delimiter incorrect
+    //UseCase 1.5 Path is correct but in csv file header incorrect
     @Test
-    public void givenIndianCensusCSVFileReturnsIncorrectDelimeter(){
+    public void givenIndianCensusCSVFileHaveIncorrectHeader() {
         CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
         try {
-            censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_PATH_DELIMETER);
-            Assert.assertTrue(true, "Wrong delimiter");
+            censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_PATH_HEADER);
+            Assert.assertTrue(true, "Wrong header in csv file");
         } catch (CensusAnalyzerException e) {
             e.printStackTrace();
         }
+
     }
 }
+
